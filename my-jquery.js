4069a10f28cf63fae5ecd2864d574d212edabc94
Tapan Kumer Das <voboghure.dev@gmail.com>
2021-02-04 19:17:37 +0600

    Update as per context (call the plugin on each li
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- my-jquery.js
+++ my-jquery.js
@@ -1,35 +1,43 @@
 (function ($) {
     $.fn.myjquery = function () {
+        if (!window.hasOwnProperty("totalLi")) {
+            window.totalLi = this.parent().children().length;
+            window.delay = 1000;
+            console.log(totalLi);
+        }
+
         // console.log(this);
-        var delay = 1000;
-        var liCount = document.querySelectorAll("li").length
+        // var liCount = document.querySelectorAll("li").length
         // console.log(liCount);
-        var i = 0;
+        // var i = 0;
 
-        $("li").each(function (index) {
-            $(this).addClass("my-custom-class");
+        // Traverse through all li and shedule ajax request
+        // $("li").each(function (index) {
+        //     $(this).addClass("my-custom-class");
 
-            postID = index + 1;
-            setTimeout(fetchdata, delay, postID, this);
-            i++;
-            j = i % 5;
-            if ( j == 0 ) {
-                delay += 1000;
-            }
-        });
+        //     postID = index + 1;
+        //     setTimeout(fetchdata, delay, postID, this);
+        //     i++;
+        //     j = i % 5;
+        //     if ( j == 0 ) {
+        //         delay += 1000;
+        //     }
+        // });
+        // console.log(this);
 
-        // $(this).addClass("my-custom-class");
+        this.addClass("my-custom-class");
+        var index = this.index() + 1;
 
-        // postID = index + 1;
-        // setTimeout(fetchdata, delay, postID, this);
-        // i++;
-        // j = i % 5;
-        // if ( j == 0 ) {
-        //     delay += 1000;
-        // }
+        setTimeout(fetchdata, window.delay, index, this);
 
-        function fetchdata(postID, li) {
-            $.ajax("https://jsonplaceholder.typicode.com/posts/" + postID, {
+        // console.log(index);
+        j = index % 5;
+        if (j == 0) {
+            window.delay += 1000;
+        }
+
+        function fetchdata(index, li) {
+            $.ajax("https://jsonplaceholder.typicode.com/posts/" + index, {
                 success: function (data, status, xhr) {
                     // console.log(li);
                     $(li).text(data["title"]);
